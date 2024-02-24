fc204c26ebabb84b3b8dcbdde28f2eb7c22f001d
GuinuxBR <guinuxbr@gmail.com>
2024-02-24 17:40:21 +0000

    Trying to fix 'install.sh'

--- install.sh
+++ install.sh
@@ -15,30 +15,34 @@ if [[ $(whoami) != "pi-star" ]]; then
 fi
 
 # Update the system and install OS dependencies
+echo ""
 echo "${GREEN}Updating the system and installing OS dependencies...${RESET}"
-sudo apt update && sudo apt upgrade -y >/dev/null 2>&1 
-sudo apt install -y git python3 python3-venv >/dev/null 2>&1
+sudo apt update
+sudo apt upgrade -y 
+sudo apt install -y git python3 python3-venv
 
 # Create the needed directories
+echo ""
 echo "${GREEN}Creating the needed directories...${RESET}"
 mkdir -p ~/git
 mkdir -p ~/logs/dapnet-ntfygateway
 mkdir -p ~/.config/systemd/user/
 
 # Clone the repository
+echo ""
 echo "${GREEN}Cloning the repository...${RESET}"
 cd ~/git || echo "${RED}Directory ~/git does not exist!${RESET}" && exit 1
-git clone https://github.com/guinuxbr/dapnet-ntfygateway.git >/dev/null 2>&1
+git clone https://github.com/guinuxbr/dapnet-ntfygateway.git
 
 # Create and activate the virtual environment
 echo "${GREEN}Creating and activating the virtual environment...${RESET}"
 cd ~/git/dapnet-ntfygateway || echo "${RED}Directory ~/git/dapnet-ntfygateway does not exist!${RESET}" && exit 1
-python3 -m venv venv >/dev/null 2>&1
-source venv/bin/activate >/dev/null 2>&1
+python3 -m venv venv
+source venv/bin/activate
 
 # Install Python dependencies
 echo "${GREEN}Installing Python dependencies...${RESET}"
-pip install -r requirements.txt >/dev/null 2>&1
+pip install -r requirements.txt
 
 # Prepare the config file
 echo "${GREEN}Preparing the config file...${RESET}"
